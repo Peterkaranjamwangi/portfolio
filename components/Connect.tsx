b5c59f163559532bf706bc822033e5186123aad4
Peter <peter@Peters-MacBook-Pro.local>
2024-02-26 13:00:48 +0300

    updates

--- components/Connect.tsx
+++ components/Connect.tsx
@@ -5,7 +5,7 @@ import { FaPhone } from 'react-icons/fa';
 const Connect = () => {
     return (
         <div>
-            <section className="mx-auto max-w-screen-md bg-gray-400 px-4 rounded-br-3xl rounded-tl-3xl py-8">
+            <section className="mx-auto max-w-screen-md bg-gray-900 px-4 rounded-br-3xl rounded-tl-3xl py-8">
                 <div className="w-full shadow-2xl mb-2 rounded-l-md flex justify-center items-center">
                     {CONNECT_DATA.map((item, index) => (
                         <div key={index} className="flex mb-2 mr-4">
@@ -17,20 +17,20 @@ const Connect = () => {
                 </div>
 
 
-                    <div className="grid rounded-2xl bg-gray-500 p-4 lg:col-span-3 lg:p-12">
+                    <div className="grid rounded-2xl bg-gray-800 p-4 lg:col-span-3 lg:p-12">
                         <span className="relative flex justify-center">
                             <div
                                 className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"
                             ></div>
 
-                            <span className="relative z-10 bg-gray-500 px-6">Send me a message</span>
+                            <span className="relative z-10 bg-gray-800 px-6">Send me a message</span>
                         </span>
 
 
                         <p className="pb-5 justify-self-center text-xl"></p>
 
 
-                            <form action="#" className="space-y-4">
+                            <form action="#" className="space-y-4 text-black">
                             <div>
                                 <label className="sr-only border" htmlFor="name">Name</label>
                                 <input
