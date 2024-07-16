d6641e9ddb20e640283774493fadf367872f5486
Peter <peter@Peters-MacBook-Pro.local>
2024-07-16 09:59:51 +0300

    download button updates

--- components/DownloadButton.tsx
+++ components/DownloadButton.tsx
@@ -1,17 +1,32 @@
+"use client";
+
 import { CloudDownloadIcon } from "lucide-react";
-import Link from "next/link";
 
 const DownloadButton: React.FC = () => {
+  const handleDownload = () => {
+    const url = "/resume.pdf";
+
+    // Create a temporary anchor element for downloading
+    const downloadLink = document.createElement("a");
+    downloadLink.href = url;
+    downloadLink.download = "resume.pdf";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+
+    // Open the file in a new tab
+    window.open(url, "_blank");
+  };
+
   return (
-    <button className="transition-all max-w-fit mt-5 duration-300 hover:bg-primary-dark justify-center items-center bg-primary text-white py-2 px-4 rounded">
-      <Link
-        href="/resume.pdf"
-        target="_blank"
-        className="text-white flex gap-2 justify-center items-center"
-      >
+    <button
+      onClick={handleDownload}
+      className="transition-all max-w-fit mt-5 duration-300 hover:bg-primary-dark justify-center items-center bg-primary text-white py-2 px-4 rounded"
+    >
+      <div className="text-white flex gap-2 justify-center items-center">
         <CloudDownloadIcon />
         Download CV
-      </Link>
+      </div>
     </button>
   );
 };
